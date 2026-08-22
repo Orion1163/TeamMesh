895ef22e61cb8f6f16ef574c2793ea91373f5286
Orion1163 <chaudharirohan2003@gmail.com>
2026-08-22 21:28:20 +0530

    first commit

---
+++ src/main/java/com/teammesh/TeamMesh/TeamMeshApplication.java
@@ -0,0 +1,13 @@
+package com.teammesh.TeamMesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamMeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teammesh/TeamMesh/TeamMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teammesh.TeamMesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
