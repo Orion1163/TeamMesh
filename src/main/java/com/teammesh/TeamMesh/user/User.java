b385e888f8dbfc2ca1dc29a098b39bf59bbccf5d
Orion1163 <chaudharirohan2003@gmail.com>
2026-08-23 13:24:17 +0530

    TeamMesh project initialized with Spring Boot and PostgreSQL configuration.

---
+++ src/main/java/com/teammesh/TeamMesh/user/User.java
@@ -0,0 +1,64 @@
+package com.teammesh.TeamMesh.user;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name="created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name="updated_at", nullable= false)
+    private Instant updatedAt;
+
+    protected User(){
+
+    }
+
+    public User(String name, String email, String password){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+}
